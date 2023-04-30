db0910353eae6197965aec5b9d816879629d5e06
WikiLiu <1307996390@qq.com>
2023-05-01 00:02:34 +0800

    first commit

--- lua/plugins/core.lua
+++ lua/plugins/core.lua
@@ -1,10 +1,11 @@
 return {
   "nvim-lua/plenary.nvim",
-  { "AstroNvim/astrotheme", opts = { plugins = { ["dashboard-nvim"] = true } } },
-  { "famiu/bufdelete.nvim", cmd = { "Bdelete", "Bwipeout" } },
-  { "max397574/better-escape.nvim", event = "InsertCharPre", opts = { timeout = 300 } },
-  { "NMAC427/guess-indent.nvim", event = "User AstroFile", config = require "plugins.configs.guess-indent" },
-  { -- TODO: REMOVE neovim-session-manager with AstroNvim v4
+  { "AstroNvim/astrotheme",         opts = { plugins = { ["dashboard-nvim"] = true } } },
+  { "famiu/bufdelete.nvim",         cmd = { "Bdelete", "Bwipeout" } },
+  { "max397574/better-escape.nvim", event = "InsertCharPre",                           opts = { timeout = 300 } },
+  { "NMAC427/guess-indent.nvim",    event = "User AstroFile",                          config = require "plugins.configs.guess-indent" },
+  {
+    -- TODO: REMOVE neovim-session-manager with AstroNvim v4
     "Shatur/neovim-session-manager",
     event = "BufWritePost",
     cmd = "SessionManager",
@@ -76,12 +77,12 @@ return {
         end
 
         return (filetype == "" or buftype == "nofile") and "indent" -- only use indent until a file is opened
-          or function(bufnr)
-            return require("ufo")
-              .getFolds(bufnr, "lsp")
-              :catch(function(err) return handleFallbackException(bufnr, err, "treesitter") end)
-              :catch(function(err) return handleFallbackException(bufnr, err, "indent") end)
-          end
+            or function(bufnr)
+              return require("ufo")
+                  .getFolds(bufnr, "lsp")
+                  :catch(function(err) return handleFallbackException(bufnr, err, "treesitter") end)
+                  :catch(function(err) return handleFallbackException(bufnr, err, "indent") end)
+            end
       end,
     },
   },
--- lua/plugins/heirline.lua
+++ lua/plugins/heirline.lua
@@ -3,6 +3,24 @@ return {
   event = "BufEnter",
   opts = function()
     local status = require "astronvim.utils.status"
+    local ViMode = require("user.vimod")
+    local utils = require("heirline.utils")
+    -- local bg_color = vim.fn.synIDattr(vim.fn.hlID("StatusLine"), "bg")
+    -- local bg_rgb = {
+    --   tonumber(bg_color:sub(2, 3), 16),
+    --   tonumber(bg_color:sub(4, 5), 16),
+    --   tonumber(bg_color:sub(6, 7), 16)
+    -- }
+    -- local lighter_rgb = {
+    --   math.max(bg_rgb[1] + 32, 0),
+    --   math.max(bg_rgb[2] + 32, 0),
+    --   math.max(bg_rgb[3] + 32, 0)
+    -- }
+    -- local lighter_color = string.format("#%02x%02x%02x",
+    --   lighter_rgb[1], lighter_rgb[2], lighter_rgb[3])
+    --
+    ViMode = utils.surround({ "", " " }, 'bg', { ViMode })
+    local Filename = require("user.filename")
     return {
       opts = {
         disable_winbar_cb = function(args)
@@ -12,13 +30,17 @@ return {
           }, args.buf)
         end,
       },
-      statusline = { -- statusline
+      statusline = {
+        -- statusline
         hl = { fg = "fg", bg = "bg" },
-        status.component.mode { mode_text = { padding = { left = 1, right = 1 } } },
+        -- status.component.mode { mode_text = { padding = { left = 1, right = 1 } } },
+        ViMode,
         status.component.git_branch(),
-        status.component.file_info { filetype = {}, filename = false, file_modified = false },
+        -- status.component.file_info { filetype = {}, filename = false, file_modified = false },
         status.component.git_diff(),
         status.component.diagnostics(),
+        status.component.mode { surround = { separator = "left" } },
+        Filename,
         status.component.fill(),
         status.component.cmd_info(),
         status.component.fill(),
@@ -27,7 +49,8 @@ return {
         status.component.nav(),
         status.component.mode { surround = { separator = "right" } },
       },
-      winbar = { -- winbar
+      winbar = {
+        -- winbar
         init = function(self) self.bufnr = vim.api.nvim_get_current_buf() end,
         fallthrough = false,
         {
@@ -45,7 +68,8 @@ return {
         status.component.breadcrumbs { hl = status.hl.get_attributes("winbar", true) },
       },
       tabline = { -- bufferline
-        { -- file tree padding
+        {
+          -- file tree padding
           condition = function(self)
             self.winid = vim.api.nvim_tabpage_list_wins(0)[1]
             return status.condition.buffer_matches(
@@ -57,14 +81,16 @@ return {
           hl = { bg = "tabline_bg" },
         },
         status.heirline.make_buflist(status.component.tabline_file_info()), -- component for each buffer tab
-        status.component.fill { hl = { bg = "tabline_bg" } }, -- fill the rest of the tabline with background color
-        { -- tab list
+        status.component.fill { hl = { bg = "tabline_bg" } },               -- fill the rest of the tabline with background color
+        {
+          -- tab list
           condition = function() return #vim.api.nvim_list_tabpages() >= 2 end, -- only show tabs if there are more than one
-          status.heirline.make_tablist { -- component for each tab
+          status.heirline.make_tablist {                                        -- component for each tab
             provider = status.provider.tabnr(),
             hl = function(self) return status.hl.get_attributes(status.heirline.tab_type(self, "tab"), true) end,
           },
-          { -- close button for current tab
+          {
+            -- close button for current tab
             provider = status.provider.close_button { kind = "TabClose", padding = { left = 1, right = 1 } },
             hl = status.hl.get_attributes("tab_close", true),
             on_click = {
